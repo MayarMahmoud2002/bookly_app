ad36664507fffd2fdecbb94d9bc4fd553990e379
Mayar Mahmoud <mahmoudmayar251@gmail.com>
2023-05-22 20:37:53 +0300

    refactor for initstate & navigate home

--- lib/constances.dart
+++ lib/constances.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color.fromRGBO(16, 11, 32, 1.0);
+const kDuration = Duration(milliseconds: 250);
--- lib/features/splash/presentation/view_models/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/view_models/widgets/splash_view_body.dart
@@ -1,5 +1,10 @@
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/views/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+import '../../../../../constances.dart';
 
 class SplashViewBody extends StatefulWidget {
   static const String routeName = "SplashViewBody";
@@ -14,19 +19,15 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
 
   @override
   void initState() {
-    animationController = AnimationController(vsync:this  , duration: const Duration(seconds: 1));
-    slidingAnimation = Tween<Offset>(begin:Offset(0, 1) , end: Offset.zero).animate(animationController);
-    animationController.forward();
-    slidingAnimation.addListener(() {
-
-      setState(() {
-
-      });
-    });
-
+    initAnimation();
+    navigateToHome();
 
   }
 
+  @override
+  void dispose() {
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,5 +45,27 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
         ),
       ],
     );
+  }
+  void navigateToHome()
+  {
+    Future.delayed(const Duration(seconds: 3),()
+    {
+      Get.to(()=> HomeScreen() , transition:Transition.rightToLeft , duration: kDuration);
+    });
+
+  }
+  void initAnimation()
+  {
+    animationController = AnimationController(vsync:this  , duration: const Duration(seconds: 1));
+    slidingAnimation = Tween<Offset>(begin:Offset(0,10) , end: Offset.zero).animate(animationController);
+    animationController.forward();
+
+
+    slidingAnimation.addListener(() {
+
+      setState(() {
+
+      });
+    });
   }
 }
